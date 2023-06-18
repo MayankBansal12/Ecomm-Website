2ac2e0e607c955baef60296930ae966fb7172fae
Mayank <mayankbansal125@gmail.com>
2023-06-18 23:56:13 +0530

    Design banner in home section

--- client/src/components/Home/Banner/Banner.jsx
+++ client/src/components/Home/Banner/Banner.jsx
@@ -1,6 +1,22 @@
+import { Link } from "react-router-dom";
 import "./Banner.scss";
+import banner from "../../../assets/banner.jpg";
+
 const Banner = () => {
-    return <div>Banner</div>;
+    return (
+        <div className="home">
+            <div className="home-primary">
+                <h1 className="home-title">Your Shopping <br /> Palace</h1>
+                <div className="home-image">
+                    <img src={banner} alt="banner" />
+                </div>
+            </div>
+            <div className="home-secondary">
+                <Link to="" className="scroll">---Scroll Down</Link>
+                <span className="home-year">2023</span>
+            </div>
+        </div>
+    )
 };
 
 export default Banner;
