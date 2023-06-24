dcc7ce34f1ba2682131c2a607bff2379cbc3b732
Mayank <mayankbansal125@gmail.com>
2023-06-25 02:28:21 +0530

    Integrate Category Page with API

--- client/src/components/Home/Category/Category.jsx
+++ client/src/components/Home/Category/Category.jsx
@@ -1,26 +1,26 @@
 import "./Category.scss";
 
-import img from "../../../assets/category/cat-1.jpg";
+// import img from "../../../assets/category/cat-1.jpg";
 
-const Category = () => {
-    return (
-        <div className="category-section">
-            <div className="categories">
-                <div className="category">
-                    <img src={img} alt="product" />
-                </div>
-                <div className="category">
-                    <img src={img} alt="product" />
-                </div>
-                <div className="category">
-                    <img src={img} alt="product" />
-                </div>
-                <div className="category">
-                    <img src={img} alt="product" />
-                </div>
-            </div>
-        </div>
-    )
+const Category = ({ categories }) => {
+  return (
+    <div className="category-section">
+      <div className="categories">
+        {categories?.data.map((item) => (
+          <div key={item.id} className="category">
+            <span>{item.attributes.title}</span>
+            <img
+              src={
+                process.env.REACT_APP_SERVER_URL +
+                item.attributes.cover.data.attributes.url
+              }
+              alt="product"
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Category;
